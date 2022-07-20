9e7e327bb1e88876757ce7e8f0fe4d5b209748fd
zihur <dog60508@gmail.com>
2022-07-20 16:04:33 +0800

    add logout button

--- src/views/Protected.vue
+++ src/views/Protected.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+import router from "@/router";
+import { ref } from "vue";
+
+const username = ref(window.user);
+const logout = () => {
+  window.user = null;
+  router.push({ name: "home" });
+};
+</script>
 <template>
-  <h1>Protected</h1>
+  <div>
+    <h1>Greetings, {{ username }}</h1>
+    <button @click="logout">Logout</button>
+  </div>
 </template>
